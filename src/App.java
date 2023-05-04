096004f1e193bfd5ae909fd621130c8a90caa5f7
serko64 <serkankabak64@gmail.com>
2023-05-04 16:41:09 +0200

    changes

--- src/App.java
+++ src/App.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        int[] input = { 5, 4, 6, 2 };
+        int[] input = { 6,3,2,7,4,-1 };
         System.out.println(permute(input));
     }
 
@@ -11,41 +11,51 @@ public class App {
         List<List<Integer>> output = new ArrayList<>();
         List<Integer> list = new ArrayList<Integer>();
 
-        int permitted = permutation(nums.length);
-
-        int currentLength = nums.length - 1;
-        int switchFirst = 0;
-        int switchIndex=0;
-
-        for (int i = 0; i < permitted; i++) {
-
-            for (int integer : nums) {
-                list.add(integer);
-            }
-
+        if (nums.length == 1) {
+            list.add(nums[0]);
             output.add(list);
-            list = new ArrayList<Integer>();
-
-            if ((currentLength - 1) == 0)
-                currentLength = nums.length - 1;
-            int temp2 = nums[currentLength];
-            nums[currentLength] = nums[currentLength - 1];
-            nums[currentLength - 1] = temp2;
-            currentLength--;
-
-            if (switchFirst == permitted / nums.length) {
-                int tempFirst = nums[0];
-                nums[0] = nums[switchIndex];
-                nums[switchIndex] = tempFirst;
-                switchFirst = 0;
-                switchIndex++;
-
-            } else {
-                switchFirst++;
-            }
-
         }
 
+        else {
+            int permitted = permutation(nums.length);
+            int currentLength = nums.length - 1;
+            int switchFirst = 0;
+            int switchIndex = 1;
+System.out.println(permitted);
+            for (int i = 0; i < permitted; i++) {
+
+                for (int integer : nums) {
+                    list.add(integer);
+                }
+                output.add(list);
+                list = new ArrayList<Integer>();
+
+                if (nums.length >= 2) {
+                    if ((currentLength - 1) == 0||(currentLength ) == 0) {
+                        currentLength = nums.length - 1;
+                    } 
+                    int temp2 = nums[currentLength];
+                    nums[currentLength] = nums[currentLength - 1];
+                    nums[currentLength - 1] = temp2;
+                    currentLength--;
+                    if (nums.length > 2) {
+                        if (switchFirst == ((permitted / nums.length) - 1)) {
+                            int tempFirst = nums[0];
+                            nums[0] = nums[switchIndex];
+                            nums[switchIndex] = tempFirst;
+                            switchFirst = 0;
+                            switchIndex++;
+                            if (switchIndex == nums.length)
+                                switchIndex = 0;
+
+                        } else {
+                            switchFirst++;
+                        }
+                    }
+                }
+            }
+        }
+       
         return output;
     }
 
--- src/AppTest.java
+++ src/AppTest.java
@@ -4,19 +4,41 @@ import static org.junit.Assert.assertEquals;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class AppTest {
 
     @Test
+    public void test() {
+        int[] test = { 1, 2 };
+        List<Integer> list2 = new ArrayList<>();
+        List<List<Integer>> listOutput2 = new ArrayList<>();
+
+        int[][] test2Result = { { 1, 2 }, { 2, 1 } };
+
+        for (int i = 0; i < test2Result.length; i++) {
+            for (int j = 0; j < test2Result[i].length; j++) {
+                list2.add(test2Result[i][j]);
+            }
+            listOutput2.add(list2);
+            list2 = new ArrayList<>();
+
+        }
+
+        assertEquals(listOutput2, App.permute(test));
+
+    }
+
+    @Test
     public void test1() {
-        int[] test1 = { 1 };
+        int[] test1 = {1};
         List<Integer> list = new ArrayList<>();
         list.add(1);
         List<List<Integer>> listOutput = new ArrayList<>();
         listOutput.add(list);
 
-        assertEquals(App.permute(test1), listOutput);
+        assertEquals(listOutput, App.permute(test1));
 
     }
 
@@ -28,15 +50,15 @@ public class AppTest {
 
         int[][] test2Result = { { 1, 2, 3 }, { 1, 3, 2 }, { 2, 1, 3 }, { 2, 3, 1 }, { 3, 1, 2 }, { 3, 2, 1 } };
 
-        for (int j = 0; j < test2Result.length; j++) {
-            for (int i = 0;i < test2Result.length; i++) {
-                list2.add(test2Result[j][i]);
-
+        for (int i = 0; i < test2Result.length; i++) {
+            for (int j = 0; j < test2Result[i].length; j++) {
+                list2.add(test2Result[i][j]);
             }
             listOutput2.add(list2);
+            list2 = new ArrayList<>();
         }
 
-        assertEquals(App.permute(test2), listOutput2);
+        assertEquals(listOutput2, App.permute(test2));
 
     }
 
@@ -44,23 +66,41 @@ public class AppTest {
     public void test3() {
         int[] test3 = { 5, 4, 6, 2 };
         List<Integer> list3 = new ArrayList<>();
-
-        int[][] test3Result = { { 5, 4, 6, 2 }, { 5, 4, 2, 6 }, { 5, 6, 4, 2 }, { 5, 6, 2, 4 }, { 5, 2, 4, 6 },
-                { 5, 2, 6, 4 }, { 4, 5, 6, 2 }, { 4, 5, 2, 6 }, { 4, 6, 5, 2 }, { 4, 6, 2, 5 }, { 4, 2, 5, 6 },
-                { 4, 2, 6, 5 }, { 6, 5, 4, 2 }, { 6, 5, 2, 4 }, { 6, 4, 5, 2 }, { 6, 4, 2, 5 }, { 6, 2, 5, 4 },
-                { 6, 2, 4, 5 }, { 2, 5, 4, 6 }, { 2, 5, 6, 4 }, { 2, 4, 5, 6 }, { 2, 4, 6, 5 }, { 2, 6, 5, 4 },
-                { 2, 6, 4, 5 } };
-        for (int i = 0; i < test3Result.length; i++) {
-
-            for (Integer integer : test3Result[i]) {
-                list3.add(integer);
-                
-            }
-        }
         List<List<Integer>> listOutput3 = new ArrayList<>();
 
-        listOutput3.add(list3);
-        assertEquals(App.permute(test3), test3Result);
+        int[][] test3Result = {{5, 4, 6, 2}, {5, 4, 2, 6}, {5, 2, 4, 6}, {5, 2, 6, 4}, {5, 6, 2, 4}, {5, 6, 4, 2}, {4, 5, 6, 2}, {4, 5, 2, 6}, {4, 2, 5, 6}, {4, 2, 6, 5}, {4, 6, 2, 5}, {4, 6, 5, 2}, {6, 5, 4, 2}, {6, 5, 2, 4}, {6, 2, 5, 4}, {6, 2, 4, 5}, 
+        {6, 4, 2, 5}, {6, 4, 5, 2}, {2, 5, 4, 6}, {2, 5, 6, 4}, {2, 6, 5, 4}, {2, 6, 4, 5}, {2, 4, 6, 5}, {2, 4, 5, 6}};
+        for (int i = 0; i < test3Result.length; i++) {
+            for (int j = 0; j < test3Result[i].length; j++) {
+                list3.add(test3Result[i][j]);
+            }
+            listOutput3.add(list3);
+            list3 = new ArrayList<>();
+
+        }
+
+        assertEquals(listOutput3, App.permute(test3));
+
+    }
+    @Ignore
+    @Test
+    public void test4() {
+        int[] test4 = { 6,3,2,7,4,-1};
+        List<Integer> list4 = new ArrayList<>();
+        List<List<Integer>> listOutput4 = new ArrayList<>();
+
+        int[][] test4Result = {{5, 4, 6, 2}, {5, 4, 2, 6}, {5, 2, 4, 6}, {5, 2, 6, 4}, {5, 6, 2, 4}, {5, 6, 4, 2}, {4, 5, 6, 2}, {4, 5, 2, 6}, {4, 2, 5, 6}, {4, 2, 6, 5}, {4, 6, 2, 5}, {4, 6, 5, 2}, {6, 5, 4, 2}, {6, 5, 2, 4}, {6, 2, 5, 4}, {6, 2, 4, 5}, 
+        {6, 4, 2, 5}, {6, 4, 5, 2}, {2, 5, 4, 6}, {2, 5, 6, 4}, {2, 6, 5, 4}, {2, 6, 4, 5}, {2, 4, 6, 5}, {2, 4, 5, 6}};
+        for (int i = 0; i < test4Result.length; i++) {
+            for (int j = 0; j < test4Result[i].length; j++) {
+                list4.add(test4Result[i][j]);
+            }
+            listOutput4.add(list4);
+            list4 = new ArrayList<>();
+
+        }
+
+        assertEquals(listOutput4, App.permute(test4));
 
     }
 }
