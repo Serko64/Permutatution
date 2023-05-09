9eeaea51a8d771e2ff626cf040c2db7783391df0
Serko64 <serkankabak64@gmail.com>
2023-05-09 10:15:50 +0200

    Changes

--- src/App.java
+++ src/App.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        int[] input = { 6, 3, 2, 7, 4, -1};
+        int[] input = { 6, 3, 2, 7, 4, -1 };
         System.out.println(permute(input));
     }
 
@@ -11,46 +11,46 @@ public class App {
         List<List<Integer>> output = new ArrayList<>();
         List<Integer> list = new ArrayList<Integer>();
 
-            int permitted = permutation(nums.length);
-            int currentLength = nums.length - 1;
-            int switchFirst = 0;
-            int switchIndex = 1;
-            for (int i = 0; i < permitted; i++) {
+        int permitted = permutation(nums.length);
+        int currentLength = nums.length - 1;
+        int switchFirst = 0;
+        int switchIndex = 1;
+        for (int i = 0; i < permitted; i++) {
 
-                for (int integer : nums) {
-                    list.add(integer);
-                }
-                
-                output.add(list);
-                list = new ArrayList<Integer>();
-
-            
-                    if ((currentLength - 1) == 0||(currentLength ) == 0) {
-                        currentLength = nums.length - 1;
-                    } 
-                    if(nums.length>1){
-                    int temp2 = nums[currentLength];
-                    nums[currentLength] = nums[currentLength - 1];
-                    nums[currentLength - 1] = temp2;
-                    currentLength--;
-                    if (nums.length > 2) {
-                        
-                        if (switchFirst == ((permitted / nums.length) - 1)) {
-                            int tempFirst = nums[0];
-                            nums[0] = nums[switchIndex];
-                            nums[switchIndex] = tempFirst;
-                            switchFirst = 0;
-                            switchIndex++;
-                            if (switchIndex == nums.length)
-                                switchIndex = 1;
-
-                        } else {
-                            switchFirst++;
-                        }
-                    }
-                    }
+            for (int integer : nums) {
+                list.add(integer);
             }
-        
+
+            output.add(list);
+            list = new ArrayList<Integer>();
+
+            if ((currentLength - 1) == 0 || (currentLength) == 0) {
+                currentLength = nums.length - 1;
+            }
+            if (nums.length > 1) {
+                int temp2 = nums[currentLength];
+                nums[currentLength] = nums[currentLength - 1];
+                nums[currentLength - 1] = temp2;
+                currentLength--;
+                
+                if (nums.length > 2) {
+
+                    if (switchFirst == ((permitted / nums.length) -1)) {
+                        int tempFirst = nums[0];
+                        nums[0] = nums[switchIndex];
+                        nums[switchIndex] = tempFirst;
+                        switchIndex++;
+                        switchFirst = 0;
+
+                    } else {
+                        switchFirst++;
+                    }
+                    if (switchIndex == nums.length)
+                        switchIndex = 1;
+                }
+            }
+        }
+
         return output;
     }
 
