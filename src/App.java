238df1ed04a54b472f3b31a94a6db21ff5c6d5e0
Serko64 <serkankabak64@gmail.com>
2023-05-10 08:48:08 +0200

    Permutation of length 6 doesnt working correctly
    [6, 3, 2, 7, 4, -1], [6, 3, 2, 7, -1, 4], [6, 3, 2, -1, 7, 4], [6, 3, -1, 2, 7, 4] 7&4 has to be permuted

--- src/App.java
+++ src/App.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        int[] input = { 6, 3, 2, 7, 4, -1 };
+        int[] input = { 6, 3, 2, 7, 4,-1 };
         System.out.println(permute(input));
     }
 
@@ -20,37 +20,56 @@ public class App {
             for (int integer : nums) {
                 list.add(integer);
             }
+/*             if(!output.contains(list))
+ */            output.add(list);
 
-            output.add(list);
             list = new ArrayList<Integer>();
 
             if ((currentLength - 1) == 0 || (currentLength) == 0) {
                 currentLength = nums.length - 1;
             }
+
             if (nums.length > 1) {
                 int temp2 = nums[currentLength];
                 nums[currentLength] = nums[currentLength - 1];
                 nums[currentLength - 1] = temp2;
-                currentLength--;
-                
+
                 if (nums.length > 2) {
 
-                    if (switchFirst == ((permitted / nums.length) -1)) {
+                    if (switchFirst == ((permitted / nums.length) - 1)) {
                         int tempFirst = nums[0];
                         nums[0] = nums[switchIndex];
                         nums[switchIndex] = tempFirst;
                         switchIndex++;
                         switchFirst = 0;
+                        if(nums.length==6)
+                        currentLength++;
+
+                        if (switchIndex == nums.length)
+                        switchIndex = 1;
 
                     } else {
                         switchFirst++;
                     }
-                    if (switchIndex == nums.length)
-                        switchIndex = 1;
-                }
-            }
-        }
 
+                }
+
+            }
+          
+            currentLength--;
+            
+        }
+        /* System.out.println("Doppelt");
+        output.stream().filter(n -> {
+            if(output.indexOf(n) != output.lastIndexOf(n)){
+                return false;
+            } else {
+                return true;
+            }
+        }).forEach(System.out::println);
+        System.out.println("Doppelt aus"); */
+
+        System.out.println(output.size());
         return output;
     }
 
