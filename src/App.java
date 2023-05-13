0438a454dcffc9f461c4223678332ce7b1bcc123
serko64 <serkankabak64@gmail.com>
2023-05-14 00:28:44 +0200

    Changes
    Deutlich weniger Fehler das Bild anwenden

--- src/App.java
+++ src/App.java
@@ -9,55 +9,61 @@ public class App {
     public static List<List<Integer>> permute(int[] nums) {
 
         List<List<Integer>> output = new ArrayList<>();
-        List<Integer> list = new ArrayList<Integer>();
 
-            int permitted = permutation(nums.length);System.out.println(permitted);
-            int currentLength = nums.length - 1;
-            int switchFirst = 0;
-            int switchIndex = 1;
-            for (int i = 0; i < permitted; i++) {
+        int permitted = permutation(nums.length);
+        int currentLength = 1;
+        int switchFirst = 0;
+        int switchIndex = 1;
+        int j=1;
+        for (int i = 0; i < permitted; i++) {
+            List<Integer> list = new ArrayList<Integer>();
 
-                for (int integer : nums) {
-                    list.add(integer);
-                }
-                
-                output.add(list);
-                list = new ArrayList<Integer>();
-
-            
-                    if ((currentLength - 1) == 0||(currentLength ) == 0) {
-                        currentLength = nums.length - 1;
-                    } 
-                    if(nums.length>1){
-                    int temp2 = nums[currentLength];
-                    nums[currentLength] = nums[currentLength - 1];
-                    nums[currentLength - 1] = temp2;
-                    currentLength--;
-                    if (nums.length > 2) {
-                        if (switchFirst == ((permitted / nums.length) - 1)) {
-                            int tempFirst = nums[0];
-                            nums[0] = nums[switchIndex];
-                            nums[switchIndex] = tempFirst;
-                            switchFirst = 0;
-                            switchIndex++;
-                            if (switchIndex == nums.length)
-                                switchIndex = 1;
-
-                        } else {
-                            switchFirst++;
-                        }
-                    }
-                    }
+            for (int integer : nums) {
+                list.add(integer);
             }
-        
-    System.out.println(output.size());
+
+            output.add(list);
+
+
+            if ((currentLength - 1) == 1|| (currentLength) == 1) {
+                currentLength = nums.length-j;
+            }
+            if (nums.length == 2) {
+                int temp2 = nums[0];
+                nums[0] = nums[1];
+                nums[1] = temp2;
+            }
+            if (nums.length > 2) {
+                int temp2 = nums[currentLength];
+                nums[currentLength] = nums[currentLength - 1];
+                nums[currentLength - 1] = temp2;
+                currentLength-=1;
+
+                if ( switchFirst == ((permitted / nums.length) - 1)) {
+                    int tempFirst = nums[0];
+                    nums[0] = nums[switchIndex];
+                    nums[switchIndex] = tempFirst;
+                    switchFirst = 0;
+                    switchIndex+=1;
+                    if (switchIndex == nums.length)
+                        switchIndex = 1;
+
+                } else {
+                    switchFirst+=1;
+                }
+            }
+          
+        }
+        System.out.println(output.size());
         return output;
+
     }
 
     public static int permutation(int i) {
-        for (int j = i - 1; j > 0; j--) {
-            i *= j;
+    int result=1;
+        for (int j = i ; j > 1; j--) {
+            result *= j;
         }
-        return i;
+        return result;
     }
 }
